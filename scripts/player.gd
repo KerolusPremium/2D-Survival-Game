5452e4709dfe6ebc8a62275297127b2cd3c6879c
KerolusPremium <kerolusashraf@gmail.com>
2024-05-13 11:31:43 +0300

    add player and world

---
+++ scripts/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+var playerState
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.x == 0 and direction.y == 0:
+		playerState = "idle"
+	elif direction.x != 0 or direction.y != 0:
+		playerState = "walking"
+	
+	velocity = direction * speed
+	#move_and_collide(velocity)
+	move_and_slide()
+	
+	playAnimation(direction)
+	
+
+func playAnimation(dir):
+	if playerState == "idle":
+		$AnimatedSprite2D.play("idle")
+	if playerState == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("n_walk")
+		if dir.x == 1:
+			$AnimatedSprite2D.play("e_walk")
+		if dir.y == 1:
+			$AnimatedSprite2D.play("s_walk")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("w_walk")
+		
+		if dir.x > 0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("ne_walk")
+		if dir.x > 0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("se_walk")
+		if dir.x < -0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("sw_walk")
+		if dir.x < -0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("nw_walk")
+		
+		
+
+func player():
+	pass
